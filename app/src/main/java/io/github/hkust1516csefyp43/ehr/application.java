556cf23dfb07e7a4aaf0f921c99907c8bd4a0fe6
Louis <louis993546@gmail.com>
2015-10-22 16:22:04 +0800

    ADD: logo and fabric

--- app/src/main/java/io/github/hkust1516csefyp43/ehr/application.java
+++ app/src/main/java/io/github/hkust1516csefyp43/ehr/application.java
@@ -1,7 +1,8 @@
 package io.github.hkust1516csefyp43.ehr;
 
-import com.parse.Parse;
-import com.parse.ParseCrashReporting;
+import com.crashlytics.android.Crashlytics;
+
+import io.fabric.sdk.android.Fabric;
 
 /**
  * Created by Louis on 1/9/15.
@@ -10,6 +11,7 @@ public class application extends android.app.Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        Fabric.with(this, new Crashlytics());
         //TODO setup a Parse account with hkust1516csefyp43@gmail.com
 //        ParseCrashReporting.enable(this);
 //        Parse.initialize(this, "LavYuwYH6JGN7MdH26XC87atMcAGgmEeSjcBdiy5", "1C9v0QxgVdwvTwOH3DlC34P9TFcLzzOiYYmM6Ix2");
