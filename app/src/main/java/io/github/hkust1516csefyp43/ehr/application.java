a9ff883185f8f93d236fe22249b3696c4a080d71
Louis Tsai <louis993546@gmail.com>
2015-11-09 18:52:38 +0800

    ADD: a library for cache

--- app/src/main/java/io/github/hkust1516csefyp43/ehr/application.java
+++ app/src/main/java/io/github/hkust1516csefyp43/ehr/application.java
@@ -1,6 +1,8 @@
 package io.github.hkust1516csefyp43.ehr;
 
 import com.crashlytics.android.Crashlytics;
+import com.vincentbrison.openlibraries.android.dualcache.lib.DualCacheContextUtils;
+import com.vincentbrison.openlibraries.android.dualcache.lib.DualCacheLogUtils;
 
 import io.fabric.sdk.android.Fabric;
 
@@ -12,5 +14,7 @@ public class application extends android.app.Application {
     public void onCreate() {
         super.onCreate();
         Fabric.with(this, new Crashlytics());
+        DualCacheLogUtils.enableLog();
+        DualCacheContextUtils.setContext(this);
     }
 }
