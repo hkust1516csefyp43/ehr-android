b576a4e250edb34f8aca378f93c74e6cb8ab2cda
Louis Tsai <louis993546@gmail.com>
2016-03-10 21:28:00 +0700

    UPDATE: logo & minor update to PMH card ui && version++

--- app/src/main/java/io/github/hkust1516csefyp43/ehr/view/viewholder/FragCardViewHolder.java
+++ app/src/main/java/io/github/hkust1516csefyp43/ehr/view/viewholder/FragCardViewHolder.java
@@ -3,7 +3,6 @@ package io.github.hkust1516csefyp43.ehr.view.viewholder;
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.TextView;
 
 import io.github.hkust1516csefyp43.ehr.R;
@@ -14,13 +13,13 @@ import io.github.hkust1516csefyp43.ehr.pojo.patient_visit.PMHFrag;
  */
 public class FragCardViewHolder extends RecyclerView.ViewHolder {
     public TextView medicineName;
-    public EditText medicalDescription;
+    public TextView medicalDescription;
     PMHFrag pmh;
 
     public FragCardViewHolder(View view, final Context context) {
         super(view);
         medicineName = (TextView) itemView.findViewById(R.id.medicineName);
-        medicalDescription = (EditText) itemView.findViewById(R.id.medicalDescription);
+        medicalDescription = (TextView) itemView.findViewById(R.id.medicalDescription);
     }
 
     public void setPmh(PMHFrag pmh) {
