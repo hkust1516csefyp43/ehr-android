622a40f2fee3a052f2b28fb69b90cf68c6ac512f
Louis Tsai <louis993546@gmail.com>
2016-03-11 13:54:01 +0700

    Minor fix after merge

--- app/src/main/java/io/github/hkust1516csefyp43/ehr/view/viewholder/FragCardViewHolder.java
+++ app/src/main/java/io/github/hkust1516csefyp43/ehr/view/viewholder/FragCardViewHolder.java
@@ -3,7 +3,6 @@ package io.github.hkust1516csefyp43.ehr.view.viewholder;
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.TextView;
 
 import io.github.hkust1516csefyp43.ehr.R;
@@ -13,14 +12,14 @@ import io.github.hkust1516csefyp43.ehr.pojo.patient_visit.PMHFrag;
  * Created by kalongip on 7/3/16.
  */
 public class FragCardViewHolder extends RecyclerView.ViewHolder {
-    public TextView medicineName;
-    public TextView medicalDescription;
+    public TextView cardTitle;
+    public TextView cardDescription;
     PMHFrag pmh;
 
     public FragCardViewHolder(View view, final Context context) {
         super(view);
-        medicineName = (TextView) itemView.findViewById(R.id.medicineName);
-        medicalDescription = (TextView) itemView.findViewById(R.id.medicalDescription);
+        cardTitle = (TextView) itemView.findViewById(R.id.medicineName);
+        cardDescription = (TextView) itemView.findViewById(R.id.medicalDescription);
     }
 
     public void setPmh(PMHFrag pmh) {
