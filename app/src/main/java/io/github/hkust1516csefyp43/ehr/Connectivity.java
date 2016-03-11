9631cc80188a5ceb125ae416ca70093ab76dd92a
Louis Tsai <louis993546@gmail.com>
2016-03-11 10:35:03 +0700

    Minor refractor

--- app/src/main/java/io/github/hkust1516csefyp43/ehr/Connectivity.java
+++ app/src/main/java/io/github/hkust1516csefyp43/ehr/Connectivity.java
@@ -8,10 +8,8 @@ import android.net.wifi.WifiManager;
 import android.telephony.TelephonyManager;
 
 /**
- * Check device's network connectivity and speed
- *
+ * Check device's network connectivity and speed (kind of)
  * @author emil http://stackoverflow.com/users/220710/emil
- *         <p/>
  *         P.S. ALWAYS give getApplicationContext()
  */
 public class Connectivity {
@@ -31,7 +29,7 @@ public class Connectivity {
      * Check if there is any connectivity
      *
      * @param context
-     * @return
+     * @return true or false
      */
     public static boolean isConnected(Context context) {
         NetworkInfo info = Connectivity.getNetworkInfo(context);
@@ -42,14 +40,19 @@ public class Connectivity {
      * Check if there is any connectivity to a Wifi network
      *
      * @param context
-     * @param type
-     * @return
+     * @return true or false
      */
     public static boolean isConnectedWifi(Context context) {
         NetworkInfo info = Connectivity.getNetworkInfo(context);
         return (info != null && info.isConnected() && info.getType() == ConnectivityManager.TYPE_WIFI);
     }
 
+    /**
+     * Get the SSID of Wi-Fi connection
+     *
+     * @param context
+     * @return
+     */
     public static String ConnectedWifiSSID(Context context) {
         if (isConnectedWifi(context)) {
             WifiManager wifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
@@ -64,7 +67,6 @@ public class Connectivity {
      * Check if there is any connectivity to a mobile network
      *
      * @param context
-     * @param type
      * @return
      */
     public static boolean isConnectedMobile(Context context) {
@@ -92,41 +94,26 @@ public class Connectivity {
      */
     public static boolean isConnectionFast(int type, int subType) {
         if (type == ConnectivityManager.TYPE_WIFI) {
-            return true;
+            return true;                                        //Is it?
         } else if (type == ConnectivityManager.TYPE_MOBILE) {
             switch (subType) {
-                case TelephonyManager.NETWORK_TYPE_1xRTT:
-                    return false; // ~ 50-100 kbps
-                case TelephonyManager.NETWORK_TYPE_CDMA:
-                    return false; // ~ 14-64 kbps
-                case TelephonyManager.NETWORK_TYPE_EDGE:
-                    return false; // ~ 50-100 kbps
-                case TelephonyManager.NETWORK_TYPE_GPRS:
-                    return false; // ~ 100 kbps
-                case TelephonyManager.NETWORK_TYPE_EVDO_0:
-                    return true; // ~ 400-1000 kbps
-                case TelephonyManager.NETWORK_TYPE_EVDO_A:
-                    return true; // ~ 600-1400 kbps
-                case TelephonyManager.NETWORK_TYPE_HSDPA:
-                    return true; // ~ 2-14 Mbps
-                case TelephonyManager.NETWORK_TYPE_HSPA:
-                    return true; // ~ 700-1700 kbps
-                case TelephonyManager.NETWORK_TYPE_HSUPA:
-                    return true; // ~ 1-23 Mbps
-                case TelephonyManager.NETWORK_TYPE_UMTS:
-                    return true; // ~ 400-7000 kbps
-                case TelephonyManager.NETWORK_TYPE_EHRPD: // API level 11
-                    return true; // ~ 1-2 Mbps
-                case TelephonyManager.NETWORK_TYPE_EVDO_B: // API level 9
-                    return true; // ~ 5 Mbps
-                case TelephonyManager.NETWORK_TYPE_HSPAP: // API level 13
-                    return true; // ~ 10-20 Mbps
-                case TelephonyManager.NETWORK_TYPE_IDEN: // API level 8
-                    return false; // ~25 kbps
-                case TelephonyManager.NETWORK_TYPE_LTE: // API level 11
-                    return true; // ~ 10+ Mbps
-                // Unknown
-                case TelephonyManager.NETWORK_TYPE_UNKNOWN:
+                case TelephonyManager.NETWORK_TYPE_LTE:         // ~ 10+ Mbps, API level 11
+                case TelephonyManager.NETWORK_TYPE_HSPAP:       // ~ 10-20 Mbps, API level 13
+                case TelephonyManager.NETWORK_TYPE_EVDO_B:      // ~ 5 Mbps, API level 9
+                case TelephonyManager.NETWORK_TYPE_HSDPA:       // ~ 2-14 Mbps
+                case TelephonyManager.NETWORK_TYPE_HSUPA:       // ~ 1-23 Mbps
+                case TelephonyManager.NETWORK_TYPE_EHRPD:       // ~ 1-2 Mbps, API level 11
+                case TelephonyManager.NETWORK_TYPE_HSPA:        // ~ 700-1700 kbps
+                    return true;
+                case TelephonyManager.NETWORK_TYPE_EVDO_A:      // ~ 600-1400 kbps
+                case TelephonyManager.NETWORK_TYPE_UMTS:        // ~ 400-7000 kbps
+                case TelephonyManager.NETWORK_TYPE_EVDO_0:      // ~ 400-1000 kbps
+                case TelephonyManager.NETWORK_TYPE_GPRS:        // ~ 100 kbps
+                case TelephonyManager.NETWORK_TYPE_EDGE:        // ~ 50-100 kbps
+                case TelephonyManager.NETWORK_TYPE_1xRTT:       // ~ 50-100 kbps
+                case TelephonyManager.NETWORK_TYPE_IDEN:        // ~ 25 kbps, API level 8
+                case TelephonyManager.NETWORK_TYPE_CDMA:        // ~ 14-64 kbps
+                case TelephonyManager.NETWORK_TYPE_UNKNOWN:     // Unknown
                 default:
                     return false;
             }
