2c84abb50c67481a6cffaca0ffece591989f1ef9
Louis Tsai <louis993546@gmail.com>
2016-03-14 15:56:24 +0700

    ADD: press back twice to exit

--- app/src/main/java/io/github/hkust1516csefyp43/ehr/view/activity/LoginActivity.java
+++ app/src/main/java/io/github/hkust1516csefyp43/ehr/view/activity/LoginActivity.java
@@ -12,6 +12,7 @@ import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.provider.ContactsContract;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -27,6 +28,10 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.crashlytics.android.answers.Answers;
+import com.crashlytics.android.answers.LoginEvent;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -42,12 +47,10 @@ import static android.Manifest.permission.READ_CONTACTS;
  * A login screen that offers login via email/password.
  */
 public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<Cursor> {
-
     /**
      * Id to identity READ_CONTACTS permission request.
      */
     private static final int REQUEST_READ_CONTACTS = 0;
-
     /**
      * A dummy authentication store containing known user names and passwords.
      * TODO: remove after connecting to a real authentication system.
@@ -55,6 +58,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     private static final String[] DUMMY_CREDENTIALS = new String[]{
             "foo@example.com:hello", "bar@example.com:world"
     };
+    boolean doubleBackToExitPressedOnce = false;
     /**
      * Keep track of the login task to ensure we can cancel it if requested.
      */
@@ -285,15 +289,28 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     public void onBackPressed() {
         //TODO press twice to leave the app
         //http://stackoverflow.com/questions/8430805/android-clicking-twice-the-back-button-to-exit-activity
-        super.onBackPressed();
+//        super.onBackPressed();
+
+        if (doubleBackToExitPressedOnce) {
+            super.onBackPressed();
+            return;
+        }
+
+        this.doubleBackToExitPressedOnce = true;
+        Toast.makeText(this, "Please click BACK again to exit", Toast.LENGTH_SHORT).show();
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                doubleBackToExitPressedOnce = false;
+            }
+        }, 2000);
     }
 
     private void addEmailsToAutoComplete(List<String> emailAddressCollection) {
         //Create adapter to tell the AutoCompleteTextView what to show in its dropdown list.
-        ArrayAdapter<String> adapter =
-                new ArrayAdapter<>(LoginActivity.this,
-                        android.R.layout.simple_dropdown_item_1line, emailAddressCollection);
-
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(LoginActivity.this, android.R.layout.simple_dropdown_item_1line, emailAddressCollection);
         mEmailView.setAdapter(adapter);
     }
 
@@ -351,6 +368,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             showProgress(false);
 
             if (success) {
+                Answers.getInstance().logLogin(new LoginEvent().putMethod("Email & password").putSuccess(true));
                 Cache.setUser(getBaseContext(), new User(mEmail, mPassword));
                 Intent i = new Intent(getApplicationContext(), TwoRecyclerViewPatientsActivity.class);
                 finish();
