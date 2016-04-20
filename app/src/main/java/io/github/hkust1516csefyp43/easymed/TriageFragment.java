fde0f834c85c36bdb75d5f160512356899ae7a8d
Louis Tsai <louis993546@gmail.com>
2016-04-20 14:13:03 +0800

    Add new patient dialog is back

--- app/src/main/java/io/github/hkust1516csefyp43/easymed/TriageFragment.java
+++ app/src/main/java/io/github/hkust1516csefyp43/easymed/TriageFragment.java
@@ -1,8 +1,11 @@
 package io.github.hkust1516csefyp43.easymed;
 
+import android.app.Dialog;
 import android.content.Context;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -15,6 +18,12 @@ import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import com.mikepenz.fontawesome_typeface_library.FontAwesome;
+import com.mikepenz.google_material_typeface_library.GoogleMaterial;
+import com.mikepenz.iconics.IconicsDrawable;
 
 import io.github.hkust1516csefyp43.easymed.listener.OnFragmentInteractionListener;
 
@@ -23,6 +32,7 @@ public class TriageFragment extends Fragment implements OnFragmentInteractionLis
   private OnFragmentInteractionListener mListener;
   private TabLayout tabLayout;
   private ViewPager viewPager;
+  private FloatingActionButton floatingActionButton;
 
   public static TriageFragment newInstance(String param1, String param2) {
     TriageFragment fragment = new TriageFragment();
@@ -79,7 +89,38 @@ public class TriageFragment extends Fragment implements OnFragmentInteractionLis
     viewPager.setAdapter(new TwoPagesAdapter(getFragmentManager()));
     viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
 
+    floatingActionButton = (FloatingActionButton) view.findViewById(R.id.fab);
+    floatingActionButton.setImageDrawable(new IconicsDrawable(getContext(), GoogleMaterial.Icon.gmd_add).color(Color.WHITE).paddingDp(3).sizeDp(16));
+    floatingActionButton.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        addPatientDialog();
+      }
+    });
+
     return view;
+  }
+
+  private void addPatientDialog() {
+    final Dialog dialog = new Dialog(getContext(), R.style.AppTheme);
+    dialog.setContentView(R.layout.dialog_app_patient);
+
+    ImageView ivExistingPatient = (ImageView) dialog.findViewById(R.id.ivExistingPatient);
+    ivExistingPatient.setImageDrawable(new IconicsDrawable(getContext(), GoogleMaterial.Icon.gmd_search).color(Color.WHITE).sizeDp(32));
+
+    ImageView ivNotSure = (ImageView) dialog.findViewById(R.id.ivNotSure);
+    ivNotSure.setImageDrawable(new IconicsDrawable(getContext(), FontAwesome.Icon.faw_question).color(Color.WHITE).sizeDp(32));
+
+    ImageView ivNewPatient = (ImageView) dialog.findViewById(R.id.ivNewPatient);
+    ivNewPatient.setImageDrawable(new IconicsDrawable(getContext(), GoogleMaterial.Icon.gmd_add).color(Color.WHITE).sizeDp(32));
+
+    ImageView ivOpenSaves = (ImageView) dialog.findViewById(R.id.ivOpenSaves);
+    ivOpenSaves.setImageDrawable(new IconicsDrawable(getContext(), GoogleMaterial.Icon.gmd_folder_open).color(getResources().getColor(R.color.secondary_text_color)).sizeDp(32));
+
+    LinearLayout llNewPatient = (LinearLayout) dialog.findViewById(R.id.llNewPatient);
+    LinearLayout llExistingPatient = (LinearLayout) dialog.findViewById(R.id.llExistingPatient);
+
+    dialog.show();
   }
 
   @Override
