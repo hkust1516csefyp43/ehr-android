f507aed11dd9f781a8962925d9823b845b5a5cf8
Louis Tsai <louis993546@gmail.com>
2016-04-22 16:45:31 +0800

    Cooming soon for settings

--- app/src/main/java/io/github/hkust1516csefyp43/easymed/view/activity/DrawerActivity.java
+++ app/src/main/java/io/github/hkust1516csefyp43/easymed/view/activity/DrawerActivity.java
@@ -8,11 +8,11 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.view.LayoutInflaterCompat;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
@@ -210,11 +210,7 @@ public class DrawerActivity extends AppCompatActivity implements NavigationView.
       FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
       fragmentTransaction.replace(R.id.fragment_container, adminFragment).commit();
     } else if (id == R.id.nav_settings) {
-      //change toolbar name
-      ActionBar actionBar = getSupportActionBar();
-      if (actionBar != null) {
-        actionBar.setTitle("Settings");
-      }
+      Snackbar.make(findViewById(android.R.id.content), "Coming soon", Snackbar.LENGTH_LONG).show();
       //swap fragment to SettingsFragment
     } else if (id == R.id.nav_about) {
       //Trigger AboutLibrary
