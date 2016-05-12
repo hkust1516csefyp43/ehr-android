2fa789e7e28f5f634bd9e4b87b88c96a36517aa6
Louis Tsai <louis993546@gmail.com>
2016-05-12 17:41:47 +0800

    FIX: display list accross time zones, bucause I am an idiot

--- app/src/main/java/io/github/hkust1516csefyp43/easymed/view/fragment/PatientListFragment.java
+++ app/src/main/java/io/github/hkust1516csefyp43/easymed/view/fragment/PatientListFragment.java
@@ -183,7 +183,7 @@ public class PatientListFragment extends Fragment{
     switch (whichPage) {
       case Const.PatientListPageId.POST_TRIAGE:
       case Const.PatientListPageId.PRE_CONSULTATION:
-        Call<List<Patient>> patientList = patientService.getPatients("1", clincId, "2", null, null, null, null, null, null, null, Util.todayStringWithTimeZone());FIX
+        Call<List<Patient>> patientList = patientService.getPatients("1", clincId, "2", null, null, null, null, null, null, null, Util.todayStringWithTimeZone());
         patientList.enqueue(new Callback<List<Patient>>() {
           @Override
           public void onResponse(Call<List<Patient>> call, Response<List<Patient>> response) {
