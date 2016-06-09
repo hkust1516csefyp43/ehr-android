a71f1b7d123361425399cdd5f35e3642478b77e7
Louis Tsai <louis993546@gmail.com>
2016-06-09 09:39:32 +0700

    EMFIX

--- app/src/main/java/io/github/hkust1516csefyp43/easymed/view/activity/PatientVisitEditActivity.java
+++ app/src/main/java/io/github/hkust1516csefyp43/easymed/view/activity/PatientVisitEditActivity.java
@@ -1714,7 +1714,7 @@ public class PatientVisitEditActivity extends AppCompatActivity implements OnFra
                       //TODO create 4 new documents
                       List<DocumentType> documentTypeList = Cache.DatabaseData.getDocumentTypes(getBaseContext());
                       for (DocumentType documentType:documentTypeList) {
-                        Call<Document> documentCall = documentService.addDocument("1", new Document("", documentType.getId(), response.body().getPatientId()));
+                        Call<Document> documentCall = documentService.addDocument("1", new Document("1", documentType.getId(), response.body().getPatientId()));
                         documentCall.enqueue(new Callback<Document>() {
                           @Override
                           public void onResponse(Call<Document> call, Response<Document> response) {
