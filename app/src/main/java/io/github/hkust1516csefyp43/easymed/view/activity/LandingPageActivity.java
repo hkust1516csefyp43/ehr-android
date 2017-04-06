79a5d939577e19b4e72b223eb9d74d05c29a3fc2
Anonhackerz6969 <tejas23682320@gmail.com>
2017-04-06 20:21:34 +0800

    Added Landing Page layout

---
+++ app/src/main/java/io/github/hkust1516csefyp43/easymed/view/activity/LandingPageActivity.java
@@ -0,0 +1,11 @@
+package io.github.hkust1516csefyp43.easymed.view.activity;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Tejas_K on 6/4/2017.
+ */
+
+public class LandingPageActivity extends AppCompatActivity {
+
+}
